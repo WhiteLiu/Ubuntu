452c5ef8123d04d5e0bb4e16d7f969b6bf6e4a4f
WhiteLiu <lingyunliushui@gmail.com>
2016-08-11 21:35:36 +0800

    abc.cpp

---
+++ Cpp/build/src/CMakeFiles/abc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/abc.dir/abc.cpp.o"
+  "../../bin/abc.pdb"
+  "../../bin/abc"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/abc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
--- Cpp/src/main.cpp
+++ Cpp/src/main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 
-int main(int argc, char**argv)
+namespace myStuff
+{ int value = 10;
+ int value2 = 20;
+int addab(int value,int value2)
 {
+return (value+value2);
+}
+int minusab(int value,int value2)
+{
+return (value-value2);
+}
+}
+
+
+
+int main()
+{
+     //using namespace myStuff;
      std::cout<<"Hello SLAM!"<<std::endl;
+     //std::cin>> value;
+     std::cout<<"\nYou entered "<<myStuff::minusab(myStuff::value,myStuff::value2)<<std::endl;
      return 0;
 }
